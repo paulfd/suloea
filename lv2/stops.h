c47fe4c693bb7cb10df2f7ed959045cf67e3f68c
Paul Fd <paul@ferrand.cc>
2021-05-09 11:55:19 +0200

    Some cleanups

---
+++ lv2/stops.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <vector>
+#include "addsynth.h"
+
+struct StopDescription
+{
+    const char* filename;
+    int del;
+    char pan;
+};
+
+const std::vector<StopDescription> stopList {
+    { "bourdon16.ae0", 20, 'C' },
+    { "flute8.ae0", 5, 'C' },
+    { "I_principal_8.ae0", 10, 'C' },
+    { "flute4.ae0", 8, 'R' },
+    { "I_principal_4.ae0", 14, 'R' },
+    { "flute2.ae0", 10, 'R' },
+    { "I_octave_2.ae0", 16, 'L' },
+    { "I_octave_1.ae0", 19, 'R' },
+    { "I_quinte_513.ae0", 21, 'C' },
+    { "I_quinte_223.ae0", 19, 'R' },
+    { "tibia8.ae0", 22, 'L' },
+    { "celesta.ae0", 13, 'C' },
+    { "I_cymbel.ae0", 4, 'C' },
+    { "I_mixtur5fach.ae0", 18, 'L' },
+    { "I_trumpet.ae0", 0, 'W' },
+};
--- lv2/suloea.cpp
+++ lv2/suloea.cpp
@@ -45,6 +45,7 @@
 #include "asection.h"
 #include "scales.h"
 #include "reverb.h"
+#include "stops.h"
 
 #include <math.h>
 #include <stdbool.h>
@@ -72,7 +73,7 @@ enum {
     FREEWHEEL_PORT
 };
 
-typedef struct
+struct SuloeaPlugin
 {
     // Features
     LV2_URID_Map* map;
@@ -104,34 +105,40 @@ typedef struct
     LV2_URID atom_string_uri;
     LV2_URID atom_bool_uri;
 
+    std::vector<Addsynth> synths;
     Reverb reverb;
     std::unique_ptr<Asection> asection;
     std::unique_ptr<Division> division;
+    unsigned char keymap[NNOTES];
     float W[PERIOD];
     float X[PERIOD];
     float Y[PERIOD];
     float Z[PERIOD];
     float R[PERIOD];
-    unsigned char keymap[NNOTES];
 
     bool activated;
     int max_block_size;
     double sample_rate;
 
+    static std::unique_ptr<SuloeaPlugin> instantiate(double rate, 
+        const char* path, const LV2_Feature* const* features);
+    // Logic taken from Aeolus
     void key_off (int n, int b)
     {
         keymap [n] &= ~b;
         keymap [n] |= 128;
     }
 
+    // Logic taken from Aeolus
     void key_on (int n, int b)
     {
         keymap [n] |= b | 128;
     }
 
-    void proc_keys1(void)
+    // Logic taken from Aeolus (proc_keys1() and proc_keys2() combined)
+    void proc_keys()
     {
-        int d, m, n;
+        int m, n;
 
         for (n = 0; n < NNOTES; n++)
         {
@@ -141,107 +148,48 @@ typedef struct
                 m &= 127;
                 keymap[n] = m;
                 division->update(n, m);
-                // for (d = 0; d < _ndivis; d++) _divisp [d]->update (n, m);
             }
         }
-    }
 
-    void proc_keys2 (void)
-    {    
-        // int d;
-    
-        // for (d = 0; d < _ndivis; d++) _divisp [d]->update (keymap);
         division->update(keymap);
     }
-} suloea_plugin_t;
 
-struct StopDescription
-{
-    std::string filename;
-    int del;
-    char pan;
-    Addsynth synth;
+    void process_midi_event(const LV2_Atom_Event* ev);
+    void check_freewheeling();
+    void map_required_uris();
+    void process_output(uint32_t sample_count);
 };
 
-std::vector<StopDescription> stopList {
-    {"I_principal_8.ae0", 10, 'C', {} },
-    {"I_principal_4.ae0", 14, 'R', {} },
-    {"I_octave_2.ae0", 16, 'L', {} },
-    {"I_octave_1.ae0", 19, 'R', {} },
-    {"I_quinte_513.ae0", 21, 'C', {} },
-    {"I_quinte_223.ae0", 19, 'R', {} },
-    {"tibia8.ae0", 22, 'L', {} },
-    {"celesta.ae0", 13, 'C', {} },
-    // {"flute8.ae0", 5, 'C', {} },
-    // {"flute4.ae0", 8, 'R', {} },
-    // {"flute2.ae0", 10, 'R', {} },
-    {"I_cymbel.ae0", 4, 'C', {} },
-    {"I_mixtur5fach.ae0", 18, 'L', {} },
-    {"I_trumpet.ae0", 0, 'W', {} },
-};
-
-static void
-suloea_map_required_uris(suloea_plugin_t* self)
+void SuloeaPlugin::map_required_uris()
 {
-    LV2_URID_Map* map = self->map;
-    self->midi_event_uri = map->map(map->handle, LV2_MIDI__MidiEvent);
-    self->max_block_length_uri = map->map(map->handle, LV2_BUF_SIZE__maxBlockLength);
-    self->nominal_block_length_uri = map->map(map->handle, LV2_BUF_SIZE__nominalBlockLength);
-    self->sample_rate_uri = map->map(map->handle, LV2_PARAMETERS__sampleRate);
-    self->atom_float_uri = map->map(map->handle, LV2_ATOM__Float);
-    self->atom_int_uri = map->map(map->handle, LV2_ATOM__Int);
-    self->atom_bool_uri = map->map(map->handle, LV2_ATOM__Bool);
-    self->atom_string_uri = map->map(map->handle, LV2_ATOM__String);
-    self->atom_urid_uri = map->map(map->handle, LV2_ATOM__URID);
+    midi_event_uri = map->map(map->handle, LV2_MIDI__MidiEvent);
+    max_block_length_uri = map->map(map->handle, LV2_BUF_SIZE__maxBlockLength);
+    nominal_block_length_uri = map->map(map->handle, LV2_BUF_SIZE__nominalBlockLength);
+    sample_rate_uri = map->map(map->handle, LV2_PARAMETERS__sampleRate);
+    atom_float_uri = map->map(map->handle, LV2_ATOM__Float);
+    atom_int_uri = map->map(map->handle, LV2_ATOM__Int);
+    atom_bool_uri = map->map(map->handle, LV2_ATOM__Bool);
+    atom_string_uri = map->map(map->handle, LV2_ATOM__String);
+    atom_urid_uri = map->map(map->handle, LV2_ATOM__URID);
 }
 
-static void
-connect_port(LV2_Handle instance,
-    uint32_t port,
-    void* data)
+std::unique_ptr<SuloeaPlugin> SuloeaPlugin::instantiate(double rate, 
+        const char* path, const LV2_Feature* const* features)
 {
-    suloea_plugin_t* self = (suloea_plugin_t*)instance;
-    switch (port) {
-    case INPUT_PORT:
-        self->input_port = (const LV2_Atom_Sequence*)data;
-        break;
-    case LEFT_BUFFER:
-        self->output_buffers[0] = (float *)data;
-        break;
-    case RIGHT_BUFFER:
-        self->output_buffers[1] = (float *)data;
-        break;
-    case FREEWHEEL_PORT:
-        self->freewheel_port = (const float *)data;
-        break;
-    default:
-        break;
-    }
-}
-
-static LV2_Handle
-instantiate(const LV2_Descriptor* descriptor,
-    double rate,
-    const char* path,
-    const LV2_Feature* const* features)
-{
-    UNUSED(descriptor);
-    UNUSED(path);
     LV2_Options_Option* options = NULL;
     bool supports_bounded_block_size = false;
     bool options_has_block_size = false;
     bool supports_fixed_block_size = false;
 
     // Allocate and initialise instance structure.
-    suloea_plugin_t* self = (suloea_plugin_t*)calloc(1, sizeof(suloea_plugin_t));
+    std::unique_ptr<SuloeaPlugin> self { new SuloeaPlugin };
     if (!self)
-        return NULL;
+        return {};
 
     // Set defaults
     self->max_block_size = MAX_BLOCK_SIZE;
     self->sample_rate = rate;
     self->activated = false;
-
 
     // Get the features from the host and populate the structure
     for (const LV2_Feature* const* f = features; *f; f++) {
@@ -272,12 +220,11 @@ instantiate(const LV2_Descriptor* descriptor,
     // The map feature is required
     if (!self->map) {
         lv2_log_error(&self->logger, "Map feature not found, aborting...\n");
-        free(self);
-        return NULL;
+        return {};
     }
 
     // Map the URIs we will need
-    suloea_map_required_uris(self);
+    self->map_required_uris();
 
     // Initialize the forge
     lv2_atom_forge_init(&self->forge, self->map);
@@ -316,20 +263,20 @@ instantiate(const LV2_Descriptor* descriptor,
     if (!supports_bounded_block_size && !supports_fixed_block_size && !options_has_block_size) {
         lv2_log_error(&self->logger,
             "Bounded block size not supported and options gave no block size, aborting...\n");
-        free(self);
-        return NULL;
+        return {};
     }
 
-    // sfizz_set_sample_rate(self->synth, self->sample_rate);
-    // sfizz_set_samples_per_block(self->synth, self->max_block_size);
-
+    memset(self->keymap, 0, NNOTES * sizeof(unsigned char));
+    self->synths.resize(stopList.size());
     std::string stopPath { path };
     stopPath += "/stops";
-    for (auto& stop: stopList) {
-        strcpy(stop.synth._filename, stop.filename.c_str());
-        stop.synth._pan = stop.pan;
-        stop.synth._del = stop.del;
-        stop.synth.load(stopPath.c_str());
+    for (unsigned i = 0; i < stopList.size(); ++i) {
+        const StopDescription& stop = stopList[i];
+        Addsynth& synth = self->synths[i];
+        strcpy(synth._filename, stop.filename);
+        synth._pan = stop.pan;
+        synth._del = stop.del;
+        synth.load(stopPath.c_str());
     }
 
     self->asection.reset(new Asection(self->sample_rate));
@@ -339,96 +286,147 @@ instantiate(const LV2_Descriptor* descriptor,
 
     // Use min/max instead of the original range. Why not?
     for (unsigned i = 0; i < stopList.size(); ++i) {
-        StopDescription& stop = stopList[i];
+        const StopDescription& stop = stopList[i];
+        Addsynth& synth = self->synths[i];
         std::unique_ptr<Rankwave> wave { new Rankwave(NOTE_MIN, NOTE_MAX) };
-        wave->gen_waves(&stop.synth, self->sample_rate, 440.0f, scales[4]._data);
+        wave->gen_waves(&synth, self->sample_rate, 440.0f, scales[4]._data);
         self->division->set_rank(i, wave.release(), stop.pan, stop.del);
+        
         // Enable the rank
         self->division->set_rank_mask(i, 128);
     }
 
-
     self->division->set_div_mask(1); // This is also the value that gets entered in key on/off
-                                     // Probably used for coupling logic overall
-
-    // self->division->set_rank(0,)
-    return (LV2_Handle)self;
+                                     // Probably used for coupling logic overall in the original
+                                     // Aeolus
+    return self;
 }
 
-static void
-cleanup(LV2_Handle instance)
-{
-    suloea_plugin_t* self = (suloea_plugin_t*)instance;
-    free(self);
-}
 
 static void
-activate(LV2_Handle instance)
+connect_port(LV2_Handle instance,
+    uint32_t port,
+    void* data)
 {
-    suloea_plugin_t* self = (suloea_plugin_t*)instance;
-    self->activated = true;
-}
-
-static void
-deactivate(LV2_Handle instance)
-{
-    suloea_plugin_t* self = (suloea_plugin_t*)instance;
-    self->activated = false;
-}
-
-static void
-process_midi_event(suloea_plugin_t* self, const LV2_Atom_Event* ev)
-{
-    UNUSED(self);
-    const uint8_t* const msg = (const uint8_t*)(ev + 1);
-    switch (lv2_midi_message_type(msg)) {
-    case LV2_MIDI_MSG_NOTE_ON:
-        if (msg[2] == 0)
-            goto noteoff; // 0 velocity note-ons should be forbidden but just in case...
-        self->key_on((int)msg[1] - NOTE_MIN, 1);
-        // self->division->update((int)msg[1] - NOTE_MIN, 1);
-        // sfizz_send_note_on(self->synth,
-        //                    (int)ev->time.frames,
-        //                    (int)msg[1],
-        //                    msg[2]);
+    SuloeaPlugin* self = (SuloeaPlugin*)instance;
+    switch (port) {
+    case INPUT_PORT:
+        self->input_port = (const LV2_Atom_Sequence*)data;
         break;
-    case LV2_MIDI_MSG_NOTE_OFF: noteoff:
-        self->key_off((int)msg[1] - NOTE_MIN, 1);
-        // self->division->update((int)msg[1] - NOTE_MIN, 0);
-        // sfizz_send_note_off(self->synth,
-        //                     (int)ev->time.frames,
-        //                     (int)msg[1],
-        //                     msg[2]);
+    case LEFT_BUFFER:
+        self->output_buffers[0] = (float *)data;
         break;
-    case LV2_MIDI_MSG_CONTROLLER:
-        // sfizz_send_cc(self->synth,
-        //               (int)ev->time.frames,
-        //               (int)msg[1],
-        //               msg[2]);
+    case RIGHT_BUFFER:
+        self->output_buffers[1] = (float *)data;
+        break;
+    case FREEWHEEL_PORT:
+        self->freewheel_port = (const float *)data;
         break;
     default:
         break;
     }
 }
 
-static void
-check_freewheeling(suloea_plugin_t* self)
+static LV2_Handle
+instantiate(const LV2_Descriptor* descriptor,
+    double rate,
+    const char* path,
+    const LV2_Feature* const* features)
 {
-    if (*(self->freewheel_port) > 0)
+    UNUSED(descriptor);
+    auto self = SuloeaPlugin::instantiate(rate, path, features);
+    return (LV2_Handle)self.release();
+}
+
+static void
+cleanup(LV2_Handle instance)
+{
+    SuloeaPlugin* self = (SuloeaPlugin*)instance;
+    free(self);
+}
+
+static void
+activate(LV2_Handle instance)
+{
+    SuloeaPlugin* self = (SuloeaPlugin*)instance;
+    self->activated = true;
+}
+
+static void
+deactivate(LV2_Handle instance)
+{
+    SuloeaPlugin* self = (SuloeaPlugin*)instance;
+    self->activated = false;
+}
+
+void SuloeaPlugin::process_midi_event(const LV2_Atom_Event* ev)
+{
+    const uint8_t* const msg = (const uint8_t*)(ev + 1);
+    switch (lv2_midi_message_type(msg)) {
+    case LV2_MIDI_MSG_NOTE_ON:
+        if (msg[2] == 0)
+            goto noteoff; // 0 velocity note-ons should be forbidden but just in case...
+        key_on((int)msg[1] - NOTE_MIN, 1);
+        break;
+    case LV2_MIDI_MSG_NOTE_OFF: noteoff:
+        key_off((int)msg[1] - NOTE_MIN, 1);
+        break;
+    default:
+        break;
+    }
+}
+
+void SuloeaPlugin::check_freewheeling()
+{
+    if (*freewheel_port > 0)
     {
-        // sfizz_enable_freewheeling(self->synth);
+        // NOOP
     }
     else
     {
-        // sfizz_disable_freewheeling(self->synth);
+        // NOOP
     }
+}
+
+void SuloeaPlugin::process_output(uint32_t sample_count)
+{
+    // Remember to set reverb parameters (see proc_synth in audio.cc)
+
+    float* out [2] { output_buffers[0], output_buffers[1] };
+    for (uint32_t k = 0; k < sample_count; k += PERIOD)
+    {
+        memset(W, 0, PERIOD * sizeof(float));
+        memset(X, 0, PERIOD * sizeof(float));
+        memset(Y, 0, PERIOD * sizeof(float));
+        memset(Z, 0, PERIOD * sizeof(float));
+        memset(R, 0, PERIOD * sizeof(float));
+
+        division->process();
+        // Volume is a default value in audio.cc init_audio() _audiopar[VOLUME]
+        asection->process(0.32f, W, X, Y, R);
+        reverb.process(PERIOD, 0.32f, R, W, X, Y, Z);
+        // Check proc_synth in audio.cc for the ambisonic version
+
+        for (unsigned j = 0; j < PERIOD; j++)
+        {
+            // Default value in audio.cc init_audio() _audiopar[STPOSIT], 
+            // seems like a stereo position
+            out[0][j] = 
+                W[j] + 0.5f * X[j] + Y[j];
+            out[1][j] =
+                W[j] + 0.5f * X[j] - Y[j];
+        }
+
+        out[0] += PERIOD;
+        out[1] += PERIOD;
+    } 
 }
 
 static void
 run(LV2_Handle instance, uint32_t sample_count)
 {
     UNUSED(sample_count);
-    suloea_plugin_t* self = (suloea_plugin_t*)instance;
+    SuloeaPlugin* self = (SuloeaPlugin*)instance;
     if (!self->activated)
         return;
 
@@ -448,45 +446,13 @@ run(LV2_Handle instance, uint32_t sample_count)
             continue;
             // Got an atom that is a MIDI event
         } else if (ev->body.type == self->midi_event_uri) {
-            process_midi_event(self, ev);
+            self->process_midi_event(ev);
         }
     }
 
-    self->proc_keys1();
-    self->proc_keys2();
-
-    check_freewheeling(self);
-
-    // Remember to set reverb parameters (see proc_synth in audio.cc)
-
-    float* out [2] { self->output_buffers[0], self->output_buffers[1] };
-    for (uint32_t k = 0; k < sample_count; k += PERIOD)
-    {
-        memset(self->W, 0, PERIOD * sizeof(float));
-        memset(self->X, 0, PERIOD * sizeof(float));
-        memset(self->Y, 0, PERIOD * sizeof(float));
-        memset(self->Z, 0, PERIOD * sizeof(float));
-        memset(self->R, 0, PERIOD * sizeof(float));
-
-        self->division->process();
-        // Volume is a default value in audio.cc init_audio() _audiopar[VOLUME]
-        self->asection->process(0.32f, self->W, self->X, self->Y, self->R);
-        self->reverb.process(PERIOD, 0.32f, self->R, self->W, self->X, self->Y, self->Z);
-        // Check proc_synth in audio.cc for the ambisonic version
-
-        for (unsigned j = 0; j < PERIOD; j++)
-        {
-            // Default value in audio.cc init_audio() _audiopar[STPOSIT], 
-            // seems like a stereo position
-            out[0][j] = 
-                self->W[j] + 0.5f * self->X[j] + self->Y[j];
-            out[1][j] =
-                self->W[j] + 0.5f * self->X[j] - self->Y[j];
-        }
-
-        out[0] += PERIOD;
-        out[1] += PERIOD;
-    } 
+    self->proc_keys();
+    self->check_freewheeling();
+    self->process_output(sample_count);
 }
 
 static uint32_t
@@ -501,7 +467,7 @@ lv2_get_options(LV2_Handle instance, LV2_Options_Option* options)
 static uint32_t
 lv2_set_options(LV2_Handle instance, const LV2_Options_Option* options)
 {
-    suloea_plugin_t* self = (suloea_plugin_t*)instance;
+    SuloeaPlugin* self = (SuloeaPlugin*)instance;
 
     // Update the block size and sample rate as needed
     for (const LV2_Options_Option* opt = options; opt->value || opt->key; ++opt) {
@@ -518,7 +484,6 @@ lv2_set_options(LV2_Handle instance, const LV2_Options_Option* options)
                 continue;
             }
             self->max_block_size = *(int*)opt->value;
-            // sfizz_set_samples_per_block(self->synth, self->max_block_size);
         }
     }
     return LV2_OPTIONS_SUCCESS;
